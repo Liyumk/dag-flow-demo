0fc050a16bd08148d01bbaf816f4f04d9a973468
Liyu <liyuumk@gmail.com>
2023-06-12 14:53:42 +0300

    page not found local image

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,12 +1,10 @@
+import bgImage from "../assets/alone.webp";
+
 const PageNotFound = () => {
   return (
     <div className="relative h-screen overflow-hidden bg-indigo-900">
-         {" "}
-      <img
-        src="https://external-preview.redd.it/4MddL-315mp40uH18BgGL2-5b6NIPHcDMBSWuN11ynM.jpg?width=960&crop=smart&auto=webp&s=b98d54a43b3dac555df398588a2c791e0f3076d9"
-        className="absolute h-full w-full object-cover"
-      />
-          <div className="absolute inset-0 bg-black opacity-25">    </div>   {" "}
+          <img src={bgImage} className="absolute h-full w-full object-cover" /> 
+        <div className="absolute inset-0 bg-black opacity-25">    </div>   {" "}
       <div className="container relative z-10 mx-auto flex items-center px-6 py-32 md:px-12 xl:py-40">
                {" "}
         <div className="relative z-10 flex w-full flex-col items-center font-mono">
