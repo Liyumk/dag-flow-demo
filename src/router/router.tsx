f3f3b798944dda321ad2d0ecef73d27526874086
Liyu <liyuumk@gmail.com>
2023-06-12 04:01:32 +0300

    page not found page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router";
 import routerConfig from "./routerConfig";
 import { routerType } from "../types/router";
 import Layout from "../components/BaseLayout";
+import PageNotFound from "../pages/PageNotFound";
 
 const Router = () => {
   const pageRoutes = routerConfig.map(
@@ -14,6 +15,7 @@ const Router = () => {
   return (
     <Routes>
       <Route Component={Layout}>{pageRoutes}</Route>
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 };
